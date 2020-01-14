8858fe92d395840e65c7005a0b918d136f892b84
GrantBirki <grant.birkinbine@gmail.com>
2020-01-13 18:21:28 -0800

    updated display again

--- Kindle/display.sh
+++ Kindle/display.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
+
+lasthashfile=/var/tmp/root/lasthash.txt
+lasttimefile=/var/tmp/root/lasttime.txt
+
 hash=$(md5sum out.png)
 time=$(stat out.png | grep "Change")
 
-lasthash=/var/tmp/root/lasthash.txt
-lasttime=/var/tmp/root/lasttime.txt
+lasthash=$(head -n 1 $lasthashfile)
+lasttime=$(head -n 1 $lasttimefile)
 
-if [ -f "$lasthash" ]
-then 
-    echo "$hash" > "$lasthash"
-
+if [ "$hash" == "$lasthash" ] && [ "$time" == "$lasttime" ]
+then
+    exit 0
+    echo "Quitting"
 else
-    touch $lasthash
-    echo "$hash" > "$lasthash"
-fi
+    if [ -f "$lasthashfile" ]
+    then 
+        echo "$hash" > "$lasthashfile"
 
-if [ -f "$lasttime" ]
-then 
-    echo "$time" > "$lasthash"
+    else
+        touch $lasthashfile
+        echo "$hash" > "$lasthashfile"
+    fi
 
-else
-    touch lasttime
-    echo "$time" > "$lasthash"
+    if [ -f "$lasttimefile" ]
+    then 
+        echo "$time" > "$lasttimefile"
+
+    else
+        touch $lasttimefile
+        echo "$time" > "$lasttimefile"
+    fi
+    echo "Displaying Image"
+    eips -g /var/tmp/root/out.png
 fi
